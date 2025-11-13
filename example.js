d7e5f5ae3810a84eccc6567e6b1cc765a2c2e9bf
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2025-11-13 14:01:14 -0500

    Completed linear 'maxAverage' problem

--- example.js
+++ example.js
@@ -9,20 +9,23 @@
  * @return {number}
  */
 function findMaxAverage(nums, k) {
+    // create a variable to save the maximum average in
     let maxAverage = -Infinity;
-    
+
+    // Loop through the array; to create k length subarrays
     for (let i = 0; i <= nums.length - k; i++) {
-        let sum = 0;
+        let sum = 0; // create a sum variable
         for (let j = i; j < i + k; j++) {
+            // iterating through the elements in the current sub array
             sum += nums[j];
         }
-        
-        const average = sum / k;
+
+        const average = sum / k; // after a sum of subarray was created, we can compare it with max
         if (average > maxAverage) {
             maxAverage = average;
         }
     }
-    
+
     return maxAverage;
 }
 
--- index.js
+++ index.js
@@ -10,8 +10,24 @@
  */
 function findMaxAverage(nums, k) {
     // TODO: Implement your solution here
-    
+    let maxAverage = -Infinity;
 
+    // you will need to sum the first 'window'
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    // you can use the first sum, average it, and compare it to max
+    maxAverage = sum / k
+
+    // create the sums, with 'sliding window' effect, using one loop
+    for (let i = 0; i < nums.length - k; i++) {
+        sum -= nums[i]
+        sum += nums[i + k]
+        maxAverage = Math.max(maxAverage, sum / k)
+    }
+    return maxAverage
 }
 
 module.exports = findMaxAverage;
